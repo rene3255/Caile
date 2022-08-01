f97b2266cbc32b24622d27759aef1ccc29c7dd8d
rene3255 <ranchoeltecal.com>
2022-08-01 16:26:56 -0500

    Fix events created report

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,8 @@ class EventsController < ApplicationController
     before_action :set_find_event, only: [:edit, :show, :update, :destroy]
   
   def index
-     @events = Event.all
+     @events = Event.select("events.title, events.description, events.event_date, events.event_time")
+     .where("user_id = #{current_user.id}")
   end
 
   def new
